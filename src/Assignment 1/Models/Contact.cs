cc8dc2cea55e6c5262ac1248cd957fdddec8be51
Dharshan Vel Velumani <dharshanvel.velumani@solitontech.com>
2026-07-16 16:16:24 +0530

    feat:resolved warnings

--- src/Assignment 1/Models/Contact.cs
+++ src/Assignment 1/Models/Contact.cs
@@ -5,23 +5,57 @@
     /// </summary>
     public class Contact
     {
-        public Guid ContactId { get; set; }
-
-        public string Name { get; set; }
-
-        public string PhoneNumber { get; set; }
-
-        public string Email { get; set; }
-
-        public string Notes { get; set; }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Contact"/> class.
+        /// Contact
+        /// </summary>
         public Contact()
         {
-            ContactId = Guid.NewGuid();
-            Name = string.Empty;
-            PhoneNumber = string.Empty;
-            Email = string.Empty;
-            Notes = string.Empty;
+            this.ContactId = Guid.NewGuid();
+            this.Name = string.Empty;
+            this.PhoneNumber = string.Empty;
+            this.Email = string.Empty;
+            this.Notes = string.Empty;
         }
+
+        /// <summary>
+        /// Gets or sets contactId
+        /// </summary>
+        /// <value>
+        ///  Contact id
+        /// </value>
+        public Guid ContactId { get; set; }
+
+        /// <summary>
+        /// Gets or sets name
+        /// </summary>
+        /// <value>
+        /// Name
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets name
+        /// </summary>
+        /// <value>
+        /// Name
+        /// </value>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets name
+        /// </summary>
+        /// <value>
+        /// Name
+        /// </value>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets name
+        /// </summary>
+        /// <value>
+        /// Name
+        /// </value>
+        public string Notes { get; set; }
     }
 }
