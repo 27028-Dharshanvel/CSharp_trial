a09d67843c7586ffd3011d317d52bac93ca79eef
Dharshan Vel Velumani <dharshanvel.velumani@solitontech.com>
2026-07-23 15:56:49 +0530

    feat:added view layer

--- src/Assignment2/Controllers/ShapeController.cs
+++ src/Assignment2/Controllers/ShapeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Assignment2.Models;
+using Assignment2.ShapeModels;
 
 namespace Assignment2.Controllers
 {
---
+++ src/Assignment2/Helpers/Helper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.Helpers
+{
+    /// <summary>
+    /// Helper clas
+    /// </summary>
+    internal class Helper
+    {
+        /// <summary>
+        /// Reads string and validate whether it is int
+        /// </summary>
+        /// <param name="message">message</param>
+        /// <returns>int</returns>
+        public static int ReadInt(string message)
+        {
+            int value;
+
+            while (true)
+            {
+                Console.Write(message);
+
+                if (int.TryParse(Console.ReadLine(), out value))
+                {
+                    return value;
+                }
+
+                Console.WriteLine("Please enter a valid integer.");
+            }
+        }
+
+        /// <summary>
+        /// Reads string and validate whether it is double
+        /// </summary>
+        /// <param name="message">message</param>
+        /// <returns>double</returns>
+        public static double ReadDouble(string message)
+        {
+            double value;
+
+            while (true)
+            {
+                Console.Write(message);
+
+                if (double.TryParse(Console.ReadLine(), out value))
+                {
+                    return value;
+                }
+
+                Console.WriteLine("Please enter a valid integer.");
+            }
+        }
+
+        /// <summary>
+        /// To display Banking Operations
+        /// </summary>
+        public static void DisplayBankingOperations()
+        {
+            Console.WriteLine("\n 1.Deposit \n 2.Withdraw");
+        }
+    }
+}
