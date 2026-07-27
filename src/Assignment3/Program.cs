028949b184d8b609c5a8762dbb2cefc9fbc56ee0
Dharshan Vel Velumani <dharshanvel.velumani@solitontech.com>
2026-07-27 16:54:44 +0530

    feat:added models

---
+++ src/Assignment3/Program.cs
@@ -0,0 +1,19 @@
+﻿using Assignment3.View;
+
+namespace Assignments
+{
+    /// <summary>
+    /// Entry Point of the Application
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Entry point of the program
+        /// </summary>
+        /// <param name="args">args</param>
+        public static void Main(string[] args)
+        {
+            ConsoleView.ConsoleOperations();
+        }
+    }
+}
---
+++ src/Assignment3/View/ConsoleView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.View
+{
+    /// <summary>
+    /// Console input and output operations
+    /// </summary>
+    internal static class ConsoleView
+    {
+        /// <summary>
+        /// Console Operations
+        /// </summary>
+        public static void ConsoleOperations()
+        {
+            Console.WriteLine("********* Inventory Management Application ************" +
+                "\n\nEnter the operation to perform : " +
+                "\n1.Add products" +
+                "\n2.View Products" +
+                "\n3.Edit product" + 
+                "\n4.Delete Product");
+            Console.ReadLine();
+        }
+    } 
+}
