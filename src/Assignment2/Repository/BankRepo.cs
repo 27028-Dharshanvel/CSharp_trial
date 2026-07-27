7c7122b511d61cd5cfb1dfe3432793427d61c063
Dharshan Vel Velumani <dharshanvel.velumani@solitontech.com>
2026-07-27 11:53:19 +0530

    feat:fixed build errors

--- src/Assignment2/Repository/BankRepo.cs
+++ src/Assignment2/Repository/BankRepo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Assignment2.BankAccountModels;
@@ -13,6 +14,34 @@ namespace Assignment2.Repository
     /// </summary>
     internal class BankRepo
     {
-        private List<BankAccount>?_bankAccounts;
+        private readonly List<BankAccount>?_bankAccounts;
+
+        /// <summary>
+        /// To get Account number
+        /// </summary>
+        /// <param name="accountNumber">accountNumber</param>
+        /// <returns>AccountNumber as string</returns>
+        public BankAccount? GetByAccountNumber(string accountNumber)
+        {
+            return _bankAccounts.FirstOrDefault(a =>  a.AccountNumber == accountNumber);
+        }
+
+        /// <summary>
+        /// Adds account to repository
+        /// </summary>
+        /// <param name="bankAccount">bankAccount</param>
+        public void Add(BankAccount bankAccount)
+        {
+            _bankAccounts.Add(bankAccount);
+        }
+
+        /// <summary>
+        /// Removes account from repository
+        /// </summary>
+        /// <param name="bankAccount">bankAccount</param>
+        public void Remove(BankAccount bankAccount)
+        {
+            _bankAccounts.Remove(bankAccount);
+        }
     }
 }
