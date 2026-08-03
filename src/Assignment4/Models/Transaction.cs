74907c603ed4443be1e518953efe2f6333be8cc2
Dharshan Vel Velumani <dharshanvel.velumani@solitontech.com>
2026-08-03 18:42:06 +0530

    feat:added models

---
+++ src/Assignment4/Models/Transaction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4.Models
+{
+    /// <summary>
+    /// Transaction
+    /// </summary>
+    internal class Transaction
+    {
+        /// <summary>
+        /// Gets or sets id
+        /// </summary>
+        /// <value>Id.</value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets amount.
+        /// </summary>
+        /// <value>Amount.</value>
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// Gets or sets Note
+        /// </summary>
+        /// <value>Id.</value>
+        public string? Description { get; set; }
+    }
+}
