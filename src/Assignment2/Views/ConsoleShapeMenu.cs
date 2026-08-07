464b1b1b5c0b070c2b5999a66b32f996a6b007e4
Dharshan Vel Velumani <dharshanvel.velumani@solitontech.com>
2026-08-07 11:18:37 +0530

    fix:implemented PrintDetails method

--- src/Assignment2/Views/ConsoleShapeMenu.cs
+++ src/Assignment2/Views/ConsoleShapeMenu.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Assignment2.Helpers;
+﻿using Assignment2.Helpers;
 using Assignment2.Models;
 
 namespace Assignment2.Views
