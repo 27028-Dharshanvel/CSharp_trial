d56c563f5dd9621d801a51faf2cf994506f5b31b
Dharshan Vel Velumani <dharshanvel.velumani@solitontech.com>
2026-08-07 15:44:08 +0530

    feat:added models

---
+++ src/Assignment4/Views/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4.Views
+{
+    /// <summary>
+    /// Main menu
+    /// </summary>
+    internal static class MainMenu
+    {
+        /// <summary>
+        /// Displays Main menu.
+        /// </summary>
+        public static void DisplayMainMenu()
+        {
+            Console.WriteLine(@"================Money manager Application====================
+
+1.Login
+2.Create new account");
+        }
+    }
+}
---
+++ src/Assignment4/Views/TransactionMenu.cs
@@ -0,0 +1,29 @@
+﻿using Assignment2.Helpers;
+using Assignment4.Models;
+
+namespace Assignment4.Views
+{
+    /// <summary>
+    /// Transaction Menu
+    /// </summary>
+    internal static class TransactionMenu
+    {
+        /// <summary>
+        /// Displays Transaction Menu
+        /// </summary>
+        public static void DisplayTransactionMenu()
+        {
+            Console.WriteLine(@"1.Add transaction
+2.View transactions
+3.Edit transaction
+4.Delete transaction
+5.View Stats");
+            TransactionMenuEnum choice = (TransactionMenuEnum)InputReader.ReadInt("\nEnter your choice : ", "Choice", 1, 6, 3, -1);
+            switch (choice)
+            {
+                case TransactionMenuEnum.AddTransaction:
+                    Console.WriteLine(""
+            }
+        }
+    }
+}
