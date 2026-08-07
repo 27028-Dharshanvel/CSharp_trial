303c66bbea9a8eeb8f22d3e71e180047496f8640
Dharshan Vel Velumani <dharshanvel.velumani@solitontech.com>
2026-08-07 18:36:24 +0530

    feat:added repository

---
+++ src/Assignment4/Repository/TransactionListRepository.cs
@@ -0,0 +1,69 @@
+﻿using Assignment4.Models;
+
+namespace Assignment4.Repository
+{
+    /// <summary>
+    /// Transaction List Repository.
+    /// </summary>
+    internal class TransactionListRepository : ITransactionRepository
+    {
+        private List<Transaction> _transactions;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TransactionListRepository"/> class.
+        /// Initializes ContactRepository.
+        /// </summary>
+        public TransactionListRepository()
+        {
+            this._transactions = new List<Transaction>();
+        }
+
+        /// <summary>
+        /// Savecontacts
+        /// </summary>
+        /// <param name="contacts">contacts</param>
+        public void SaveContacts(List<Transaction> contacts)
+        {
+            this._transactions = contacts;
+        }
+
+        /// <summary>
+        /// Loads Contacts.
+        /// </summary>
+        /// <returns>list of contacts.</returns>
+        public List<Transaction> LoadContacts()
+        {
+            return this._transactions;
+        }
+
+        /// <summary>
+        /// Adds Contacts.
+        /// </summary>
+        /// <param name="contact">contact</param>
+        public void AddContact(Transaction contact)
+        {
+            this._transactions.Add(contact);
+        }
+
+        /// <summary>
+        /// Deletes Contacts.
+        /// </summary>
+        /// <param name="contact">contact</param>
+        public void DeleteContact(Transaction contact)
+        {
+            this._transactions.Remove(contact);
+        }
+
+        /// <summary>
+        /// Update Contacts.
+        /// </summary>
+        /// <param name="oldTransaction">oldcontact</param>
+        /// <param name="updatedTransaction">updatedContact</param>
+        public void UpdateContact(Transaction oldTransaction, Transaction updatedTransaction)
+        {
+            oldTransaction.Amount = updatedTransaction.Amount;
+            oldTransaction.Date = updatedTransaction.Date;
+            oldTransaction.Category = updatedTransaction.Category;
+        }
+    }
+}
--- src/Assignment4/Services/Service.cs
+++ src/Assignment4/Services/Service.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace Assignment4.Services
 {
+    /// <summary>
+    /// Service Class
+    /// </summary>
     internal class Service
     {
+        /// <summary>
+        /// Adds Transaction to the repository.
+        /// </summary>
+        /// <param name="amount">amount</param>
+        /// <param name="category">category</param>
+        /// <param name="date">date</param>
+        /// <returns>True if transaction is added, False if error.</returns>
+        public bool AddTransaction(decimal amount, string category, DateOnly date)
+        {
+            
+        }
     }
 }
