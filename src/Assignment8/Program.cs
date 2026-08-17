28a719ba60a4faf458a6de5a04be718c16f87ad1
Dharshan Vel Velumani <dharshanvel.velumani@solitontech.com>
2026-08-17 19:23:53 +0530

    feat:implemented error handling

---
+++ src/Assignment8/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Assignments
+{
+    /// <summary>
+    /// Entry point of the program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
