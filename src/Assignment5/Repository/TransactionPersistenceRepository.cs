7e551d55b86b31d1efe896a1937fd5da648b5111
Dharshan Vel Velumani <dharshanvel.velumani@solitontech.com>
2026-08-17 19:12:04 +0530

    feat:added persistence repository

---
+++ src/Assignment5/Repository/TransactionPersistenceRepository.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Assignment5.Models;
+
+namespace Assignment5.Repository
+{
+    /// <summary>
+    /// Transaction Repository.
+    /// </summary>
+    internal class TransactionPersistenceRepository : ITransactionRepository
+    {
+        private readonly string _filePath = "transactions.json";
+
+        /// <summary>
+        /// Add transactions.
+        /// </summary>
+        /// <param name="transaction">transaction</param>
+        public void AddTransaction(Transaction transaction)
+        {
+            var transactions = LoadTransactions();
+            transactions.Add(transaction);
+            SaveTransactions(transactions);
+        }
+
+        /// <summary>
+        /// Deletes transaction.
+        /// </summary>
+        /// <param name="transaction">transaction</param>
+        /// <exception cref="NotImplementedException">Notimplemented exception.</exception>
+        public void DeleteTransaction(Transaction transaction)
+        {
+
+            var transactions = LoadTransactions();
+
+            transactions.RemoveAll(u => u.TransactionId == transaction.TransactionId);
+
+            this.SaveTransactions(transactions);
+        }
+
+        /// <summary>
+        /// Loads transactions.
+        /// </summary>
+        /// <returns>list of transactions.</returns>
+        /// <exception cref="NotImplementedException">notimplemented exception.</exception>
+        public List<Transaction> LoadTransactions()
+        {
+            if(!File.Exists(_filePath))
+            {
+                return new List<Transaction>();
+            }
+
+            string json = File.ReadAllText(_filePath);
+
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                return new List<Transaction>();
+            }
+
+            return JsonSerializer.Deserialize<List<Transaction>>(json)
+                   ?? new List<Transaction>();
+        }
+
+        /// <summary>
+        /// Saves transactions.
+        /// </summary>
+        /// <param name="transactions">transaction</param>
+        /// <exception cref="NotImplementedException">not implemented exception.</exception>
+        public void SaveTransactions(List<Transaction> transactions)
+        {
+            string json = JsonSerializer.Serialize(
+                transactions,
+                new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                });
+
+            File.WriteAllText(_filePath, json);
+        }
+
+        /// <summary>
+        /// Update transacions.
+        /// </summary>
+        /// <param name="oldTransaction">old transaction.</param>
+        /// <param name="updatedTransaction">updated transaction.</param>
+        /// <exception cref="NotImplementedException">not implemented exception.</exception>
+        public void UpdateTransaction(Transaction oldTransaction, Transaction updatedTransaction)
+        {
+            var transactions = LoadTransactions();
+
+            int index = transactions.FindIndex(u => u.TransactionId == oldTransaction.TransactionId);
+
+            if (index == -1)
+            {
+                throw new InvalidOperationException("User not found.");
+            }
+
+            transactions[index] = updatedTransaction;
+
+            SaveTransactions(transactions);
+        }
+    }
+}
---
+++ src/Assignment5/Repository/UserPersistenceRepository.cs
@@ -0,0 +1,97 @@
+﻿using System.Text.Json;
+using Assignment5.Models;
+
+namespace Assignment5.Repository
+{
+    /// <summary>
+    /// User repository with persistence.
+    /// </summary>
+    internal class UserPersistenceRepository : IUserRepository
+    {
+        private readonly string _filePath = "users.json";
+
+        /// <summary>
+        /// Adds a new user to the repository.
+        /// </summary>
+        /// <param name="user">user</param>
+        public void AddUser(User user)
+        {
+            var users = LoadUsers();
+            users.Add(user);
+            SaveUsers(users);
+        }
+
+        /// <summary>
+        /// Deletes user
+        /// </summary>
+        /// <param name="user">user</param>
+        public void DeleteUser(User user)
+        {
+            var users = LoadUsers();
+
+            users.RemoveAll(u => u.UserId == user.UserId);
+
+            this.SaveUsers(users);
+        }
+
+        /// <summary>
+        /// Loads user
+        /// </summary>
+        /// <returns>list of users.</returns>
+        public List<User> LoadUsers()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return new List<User>();
+            }
+
+            string json = File.ReadAllText(_filePath);
+
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                return new List<User>();
+            }
+
+            return JsonSerializer.Deserialize<List<User>>(json)
+                   ?? new List<User>();
+        }
+
+        /// <summary>
+        /// Saves users.
+        /// </summary>
+        /// <param name="users">list of users.</param>
+        public void SaveUsers(List<User> users)
+        {
+            string json = JsonSerializer.Serialize(
+                users,
+                new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                });
+
+            File.WriteAllText(_filePath, json);
+        }
+
+        /// <summary>
+        /// Updates user.
+        /// </summary>
+        /// <param name="oldUser">oldUser</param>
+        /// <param name="updatedUser">updatedUser</param>
+        /// <exception cref="InvalidOperationException">InvalidOperationException.</exception>
+        public void UpdateUser(User oldUser, User updatedUser)
+        {
+            var users = LoadUsers();
+
+            int index = users.FindIndex(u => u.UserId == oldUser.UserId);
+
+            if (index == -1)
+            {
+                throw new InvalidOperationException("User not found.");
+            }
+
+            users[index] = updatedUser;
+
+            SaveUsers(users);
+        }
+    }
+}
