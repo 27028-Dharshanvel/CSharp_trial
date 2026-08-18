4622db6d2ea83d106542d1a257e33c32973ebf06
Dharshan Vel Velumani <dharshanvel.velumani@solitontech.com>
2026-08-18 10:20:43 +0530

    feat:refactored inputreader

--- src/Assignment3/Helpers/ConsoleInputReader.cs
+++ src/Assignment3/Helpers/ConsoleInputReader.cs
@@ -6,180 +6,183 @@
     internal class ConsoleInputReader
     {
         /// <summary>
-        /// Reads string and validate whether it is an integer,within range and with limited tries.
+        /// Generic delegate for TryParse methods.
         /// </summary>
-        /// <param name="message">Prompt for the user.</param>
-        /// <param name="inputType">Variable name of the input </param>
-        /// <param name="minValue">Inclusive lower bound of the range.</param>
-        /// <param name="maxValue">Exclusive upper bound of the range.</param>
-        /// <param name="maxTries">Maximum Tries user can make.</param>
-        /// <param name="defaultValue">Default integer value that will be returned.</param>
-        /// <returns>Integer</returns>
-        public static int ReadInt(string message, string inputType, int minValue, int maxValue, int maxTries, int defaultValue)
-        {
-            int value;
-
-            while (maxTries > 0)
-            {
-                Console.Write(message);
-
-                if (int.TryParse(Console.ReadLine(), out value))
-                {
-                    if (value >= minValue && value < maxValue)
-                    {
-                        return value;
-                    }
-                    else
-                    {
-                        maxTries--;
-                        if (maxTries == 0)
-                        {
-                            continue;
-                        }
-
-                        ConsoleOutputColor.Warn($"{inputType} should be in range of {minValue} and {maxValue}");
-                        continue;
-                    }
-                }
-
-                maxTries--;
-                if (maxTries == 0)
-                {
-                    continue;
-                }
-
-                ConsoleOutputColor.Warn("Please enter a valid integer.");
-            }
-
-            ConsoleOutputColor.Error("\nToo many Attempts! Try again later");
-            return defaultValue;
-        }
+        private delegate bool TryParseDelegate<T>(string? input, out T value);
 
         /// <summary>
-        /// Reads string and validate whether it is an double,within range and with limited tries.
+        /// Reads and validates string input.
         /// </summary>
-        /// <param name="message">Prompt for the user.</param>
-        /// <param name="inputType">Variable name of the input </param>
-        /// <param name="minValue">Inclusive lower bound of the range.</param>
-        /// <param name="maxValue">Exclusive upper bound of the range.</param>
-        /// <param name="maxTries">Maximum Tries user can make.</param>
-        /// <param name="defaultValue">Default value that will be returned.</param>
-        /// <returns>Double</returns>
-        public static double ReadDouble(string message, string inputType, double minValue, double maxValue, int maxTries, double defaultValue)
-        {
-            double value;
-
-            while (maxTries > 0)
-            {
-                Console.Write(message);
-
-                if (double.TryParse(Console.ReadLine(), out value))
-                {
-                    if (value >= minValue && value < maxValue)
-                    {
-                        return value;
-                    }
-                    else
-                    {
-                        maxTries--;
-                        if (maxTries == 0)
-                        {
-                            continue;
-                        }
-
-                        ConsoleOutputColor.Warn($"{inputType} should be in range of {minValue} and {maxValue}");
-                        continue;
-                    }
-                }
-
-                maxTries--;
-                if (maxTries == 0)
-                {
-                    continue;
-                }
-
-                ConsoleOutputColor.Warn("Please enter a valid double.");
-            }
-
-            ConsoleOutputColor.Error("\nToo many Attempts! Try again later");
-            return defaultValue;
-        }
-
-        /// <summary>
-        /// Reads string and validate whether it is an decimal,within range and with limited tries.
-        /// </summary>
-        /// <param name="message">Prompt for the user.</param>
-        /// <param name="inputType">Variable name of the input </param>
-        /// <param name="minValue">Inclusive lower bound of the range.</param>
-        /// <param name="maxValue">Exclusive upper bound of the range.</param>
-        /// <param name="maxTries">Maximum Tries user can make.</param>
-        /// <param name="defaultValue">Default value that will be returned.</param>
-        /// <returns>Decimal</returns>
-        public static decimal ReadDecimal(string message, string inputType, decimal minValue, decimal maxValue, int maxTries, decimal defaultValue)
-        {
-            decimal value;
-
-            while (maxTries > 0)
-            {
-                Console.Write(message);
-
-                if (decimal.TryParse(Console.ReadLine(), out value))
-                {
-                    if (value >= minValue && value < maxValue)
-                    {
-                        return value;
-                    }
-                    else
-                    {
-                        maxTries--;
-                        if (maxTries == 0)
-                        {
-                            continue;
-                        }
-
-                        ConsoleOutputColor.Warn($"{inputType} should be in range of {minValue} and {maxValue}");
-                        continue;
-                    }
-                }
-
-                maxTries--;
-                if (maxTries == 0)
-                {
-                    continue;
-                }
-
-                ConsoleOutputColor.Warn("Please enter a valid decimal.");
-            }
-
-            ConsoleOutputColor.Error("\nToo many Attempts! Try again later");
-            return defaultValue;
-        }
-
-        /// <summary>
-        /// Reads string and validate null values.
-        /// </summary>
-        /// <param name="message">Prompt for the user.</param>
-        /// <param name="value">Variable name of the value</param>
-        /// <param name="maxCharacters">Maximum characters that can be entered.</param>
-        /// <param name="maxTries">Maximum tries.</param>
-        /// <param name="defaultValue">Default string that will be returned.</param>
-        /// <returns>string.</returns>
-        public static string ReadString(string message, string value, int maxCharacters, int maxTries, string defaultValue)
+        /// <param name="message">message</param>
+        /// <param name="valueName">valueName</param>
+        /// <param name="maxCharacters">maxcharacters</param>
+        /// <param name="maxTries">maxtries</param>
+        /// <param name="defaultValue">defautlvalue</param>
+        /// <returns>string</returns>
+        public static string ReadString(
+            string message,
+            string valueName,
+            int maxCharacters,
+            int maxTries,
+            string defaultValue)
         {
             while (maxTries > 0)
             {
                 Console.Write(message);
-                string input = Console.ReadLine();
 
-                if (!string.IsNullOrWhiteSpace(input) && input.Length <= maxCharacters)
+                string? input = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(input) &&
+                    input.Length <= maxCharacters)
                 {
                     return input;
                 }
 
                 maxTries--;
-                ConsoleOutputColor.Warn($"{value} should neither be Null nor exceed {maxCharacters} characters.");
+
+                if (maxTries > 0)
+                {
+                    ConsoleOutputColor.Warn(
+                        $"{valueName} should neither be null nor exceed {maxCharacters} characters.");
+                }
             }
 
+            ConsoleOutputColor.Error("\nToo many Attempts! Try again later");
+            return defaultValue;
+        }
+
+        /// <summary>
+        /// Reads and validates integer input.
+        /// </summary>
+        /// <param name="message">message</param>
+        /// <param name="inputType">inputytpe</param>
+        /// <param name="minValue">minvalue</param>
+        /// <param name="maxValue">maxvalue</param>
+        /// <param name="maxTries">maxtries</param>
+        /// <param name="defaultValue">defaultvalue</param>
+        /// <returns>integer</returns>
+        public static int ReadInt(
+            string message,
+            string inputType,
+            int minValue,
+            int maxValue,
+            int maxTries,
+            int defaultValue)
+        {
+            return ReadNumber(
+                message,
+                inputType,
+                minValue,
+                maxValue,
+                maxTries,
+                defaultValue,
+                int.TryParse);
+        }
+
+        /// <summary>
+        /// Reads and validates double input.
+        /// </summary>
+        /// /// <param name="message">message</param>
+        /// <param name="inputType">inputytpe</param>
+        /// <param name="minValue">minvalue</param>
+        /// <param name="maxValue">maxvalue</param>
+        /// <param name="maxTries">maxtries</param>
+        /// <param name="defaultValue">defaultvalue</param>
+        /// <returns>integer</returns>
+        public static double ReadDouble(
+            string message,
+            string inputType,
+            double minValue,
+            double maxValue,
+            int maxTries,
+            double defaultValue)
+        {
+            return ReadNumber(
+                message,
+                inputType,
+                minValue,
+                maxValue,
+                maxTries,
+                defaultValue,
+                double.TryParse);
+        }
+
+        /// <summary>
+        /// Reads and validates decimal input.
+        /// </summary>
+        /// /// <param name="message">message</param>
+        /// <param name="inputType">inputytpe</param>
+        /// <param name="minValue">minvalue</param>
+        /// <param name="maxValue">maxvalue</param>
+        /// <param name="maxTries">maxtries</param>
+        /// <param name="defaultValue">defaultvalue</param>
+        /// <returns>integer</returns>
+        public static decimal ReadDecimal(
+            string message,
+            string inputType,
+            decimal minValue,
+            decimal maxValue,
+            int maxTries,
+            decimal defaultValue)
+        {
+            return ReadNumber(
+                message,
+                inputType,
+                minValue,
+                maxValue,
+                maxTries,
+                defaultValue,
+                decimal.TryParse);
+        }
+
+        /// <summary>
+        /// Generic method for reading numeric values.
+        /// </summary>
+        private static T ReadNumber<T>(
+            string message,
+            string inputType,
+            T minValue,
+            T maxValue,
+            int maxTries,
+            T defaultValue,
+            TryParseDelegate<T> tryParse)
+            where T : IComparable<T>
+        {
+            while (maxTries > 0)
+            {
+                Console.Write(message);
+
+                string? input = Console.ReadLine();
+
+                if (tryParse(input, out T value))
+                {
+                    if (value.CompareTo(minValue) >= 0 &&
+                        value.CompareTo(maxValue) < 0)
+                    {
+                        return value;
+                    }
+
+                    maxTries--;
+
+                    if (maxTries > 0)
+                    {
+                        ConsoleOutputColor.Warn(
+                            $"{inputType} should be in range of {minValue} and {maxValue}");
+                    }
+
+                    continue;
+                }
+
+                maxTries--;
+
+                if (maxTries > 0)
+                {
+                    ConsoleOutputColor.Warn(
+                        $"Please enter a valid {typeof(T).Name.ToLower()}.");
+                }
+            }
+
+            ConsoleOutputColor.Error("\nToo many Attempts! Try again later");
             return defaultValue;
         }
     }
