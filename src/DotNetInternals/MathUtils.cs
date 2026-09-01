5366b047afc1fe0f9403e6a8333ab3881266a590
Dharshan Vel Velumani <dharshanvel.velumani@solitontech.com>
2026-09-01 10:23:57 +0530

    feat:added MathUtils

---
+++ src/DotNetInternals/MathUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetInternals
+{
+    /// <summary>
+    /// Mathematic operations
+    /// </summary>
+    internal class MathUtils
+    {
+    }
+}
