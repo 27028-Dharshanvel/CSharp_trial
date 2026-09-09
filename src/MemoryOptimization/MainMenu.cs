7eeaccc682ff1fcceea2a0e746930d6473d5da20
Dharshan Vel Velumani <dharshanvel.velumani@solitontech.com>
2026-09-09 20:06:00 +0530

    feat:added memory optimization

---
+++ src/MemoryOptimization/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using Assignments;
+
+namespace MemoryOptimization
+{
+    /// <summary>
+    /// Main menu 
+    /// </summary>
+    internal class MainMenu
+    {
+        /// <summary>
+        /// Displays the main menu.
+        /// </summary>
+        public void DisplayMainMenu()
+        {
+            Console.WriteLine(@"---------Memory Optimaztion--------------
+
+1.Diagnose memory issues in the code
+2.Fix and implement memory management best practices
+3.Optimize and compare the corrected code and original code
+
+Choose a task to demonstrate : ");
+
+            int choice;
+            if (int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine($"Index: {choice}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    MemoryEater me = new MemoryEater();
+                    me.Allocate();
+                    break;
+
+                case 2:
+                    // TO DO : Memory optimization.
+                    break;
+                case 3:
+                    // TO DO : Memory Comparison.
+                    break;
+            }
+        }
+    }
+}
---
+++ src/MemoryOptimization/MemoryEater.cs
@@ -0,0 +1,37 @@
+﻿using MemoryOptimization;
+
+namespace Assignments
+{
+
+    /// <summary>
+    /// Memory eater class
+    /// </summary>
+    internal class MemoryEater
+    {
+        private List<int[]> _memAlloc = new List<int[]>();
+
+        /// <summary>
+        /// Allocate method
+        /// </summary>
+        public void Allocate()
+        {
+            while (true)
+            {
+                this._memAlloc.Add(new int[1000]);
+                Thread.Sleep(10);
+            }
+        }
+
+        /// <summary>
+        /// Allocate method
+        /// </summary>
+        public void AllocateWithOptimization()
+        {
+            while (true)
+            {
+                this._memAlloc.Add(new int[1000]);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
