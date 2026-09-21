c4e38a767cbb9dc4d3c4274730e3f815462d22b6
Dharshan Vel Velumani <dharshanvel.velumani@solitontech.com>
2026-09-21 12:15:27 +0530

    refactor:refactored input validation

---
+++ src/MemoryManagement/ValueAndReferenceTypes/InputValidator.cs
@@ -0,0 +1,31 @@
+﻿namespace ValueAndReferenceTypes
+{
+    /// <summary>
+    /// Input validator
+    /// </summary>
+    internal static class InputValidator
+    {
+        /// <summary>
+        /// Validates whether a input string in an integer.
+        /// </summary>
+        /// <param name="input">input value</param>
+        /// <param name="result">out integer result</param>
+        /// <returns>True if within range, False otherwise.</returns>
+        public static bool IsValidInt(string input, out int result)
+        {
+            return int.TryParse(input, out result);
+        }
+
+        /// <summary>
+        /// Validates whether input integer is within range
+        /// </summary>
+        /// <param name="value">input value</param>
+        /// <param name="min">minimum value</param>
+        /// <param name="max">maximum value</param>
+        /// <returns>True if within range, False otherwise.</returns>
+        public static bool IsIntWithinRange(int value, int min, int max)
+        {
+            return value >= min && value <= max;
+        }
+    }
+}
--- src/MemoryManagement/ValueAndReferenceTypes/Program.cs
+++ src/MemoryManagement/ValueAndReferenceTypes/Program.cs
@@ -14,17 +14,28 @@ internal class Program
         bool isAppRunning = true;
         while (isAppRunning)
         {
-            Console.Write(@"------------Value and Refernce types-----------
+            Console.Clear();
+            Console.Write(@"------------Value and Reference types-----------
 
-1.Demonstrate modification of a value and refernce type.
+1.Demonstrate modification of a value and reference type.
 2.Perform allocation of a large array of integers.
 3.Perform calculation with a large number of local variables.
 4.Exit application.
 
 Enter your choice : ");
-            if (!int.TryParse(Console.ReadLine(), out int choice))
+
+            if (!InputValidator.IsValidInt(Console.ReadLine(), out int choice))
             {
                 Console.WriteLine("Please enter a valid choice");
+                Console.ReadKey();
+                continue;
+            }
+
+            if (!InputValidator.IsIntWithinRange(choice, 1, 4))
+            {
+                Console.WriteLine("Select a choice btween 1-4");
+                Console.ReadKey();
+                continue;
             }
 
             switch (choice)
@@ -34,7 +45,7 @@ Enter your choice : ");
                     Student student = new Student("Dharshan");
                     Console.WriteLine(@$"Before Modification : 
 Integer (Value type) = {sampleInteger}
-student.Name (Refernce type) = {student.Name}");
+student.Name (Reference type) = {student.Name}");
 
                     ModifyInputs(sampleInteger, student);
 
@@ -47,7 +58,7 @@ Refernce type is Modified : {student.Name}");
                 case 2:
                     Console.WriteLine(@"Performs allocation of a large array of inetegers.
 View diagnostic tools to analyze memory usage");
-                    Console.WriteLine(AllocateLargeArray(10000000));
+                    Console.WriteLine(AllocateLargeArray(100000));
                     Console.ReadKey();
                     break;
 
@@ -65,7 +76,6 @@ View diagnostic tools to analyze memory usage");
             }
 
             Console.ReadKey();
-            Console.Clear();
         }
     }
 
